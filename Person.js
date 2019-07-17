b40197db32590e57f2d8d215f6014b52e40450b5
xuliwennew <624026015@qq.com>
2019-07-17 18:22:30 +0800

    react-jsx

---
+++ Person.js
@@ -0,0 +1,25 @@
+
+
+class Person {
+
+    //构造方法
+
+    //属性 实例属性 类属性(静态)
+
+    name = "zhansan" //getter setter
+    static tags = ["会思考"]
+    //方法 实例方法 类方法
+
+    say(){
+
+    }
+
+    static sleep(){
+
+    }
+}
+
+
+let zs = new Person()
+console.log(zs.name)
+
